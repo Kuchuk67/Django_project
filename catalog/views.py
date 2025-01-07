ab18379396596a46d03de2de7915a61abe55e866
Serg_Nout <kuchukov.sergey@gmail.com>
2025-01-07 12:55:56 +0300

    add post metod

--- catalog/views.py
+++ catalog/views.py
@@ -6,6 +6,14 @@ def home(request):
     return render(request, 'home.html')
 
 def contact(request):
-    #print({{ request.path }})
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(name, email, message)
+        request.status_post = True
+    else:
+        request.status_post = False
     return render(request, 'contact.html')
 
