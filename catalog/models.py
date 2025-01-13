ce98e2fb533b69799f23cdd831314da5329ad7f7
Serg_Nout <kuchukov.sergey@gmail.com>
2025-01-13 23:15:29 +0300

    task 5

--- catalog/models.py
+++ catalog/models.py
@@ -19,7 +19,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование')
     description = models.TextField(verbose_name='описание')
-    image = models.ImageField(upload_to='images/', verbose_name='изображение')
+    image = models.ImageField(upload_to='images_products/', null=True, blank=True, verbose_name='изображение')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
     price = models.IntegerField( verbose_name='цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
