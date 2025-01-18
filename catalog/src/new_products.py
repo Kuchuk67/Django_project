e1e94f00e6a60f3ffc9923a0174c932ee34f6a7f
Serg_Nout <kuchukov.sergey@gmail.com>
2025-01-18 10:39:33 +0300

    fixed

--- catalog/src/new_products.py
+++ catalog/src/new_products.py
@@ -6,6 +6,7 @@ def new_product() -> dict:
     all_cat =  Category.objects.all()
     data = {}
     dict_products = {}
+    data_showcase_id = []
     for category in all_cat:
         #print(category.id)
         products = Product.objects.order_by('-id').filter( category=category.id)[:1] #
@@ -15,12 +16,14 @@ def new_product() -> dict:
                                          'image': product.image,
                                          'price': product.price,
                                          'id': product.id,}
-
+            data_showcase_id.append(product.id)
         sorted_dict_products = dict(sorted(dict_products.items(), reverse=True))
         i = 0
 
+
         for value in sorted_dict_products.values():
             i+=1
+            # data_showcase.append([value['id'], value['name'], value['description'], value['image'], value['price']])
             data['name'+str(i)] = value['name']
             data['description' + str(i)] = value['description']
             data['image' + str(i)] = value['image']
@@ -29,8 +32,14 @@ def new_product() -> dict:
             if i == 9:
                 break
 
+
+    data_showcase_id.sort(reverse=True)
+    data['showcase_product'] = []
+    data['showcase_product'].append(Product.objects.filter(id__in = data_showcase_id[0:3]))
+    data['showcase_product'].append(Product.objects.filter(id__in=data_showcase_id[3:6]))
+    data['showcase_product'].append(Product.objects.filter(id__in=data_showcase_id[6:9]))
     return data
-    #return sorted(dict_products, reverse=True)
+
 
 def data_product() -> dict:
     ...
