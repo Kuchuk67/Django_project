d949fc3b09b9c37425395a4361bba10b98f617fb
Serg_Nout <kuchukov.sergey@gmail.com>
2025-01-23 23:55:40 +0300

    fixed extra_context

--- catalog/views.py
+++ catalog/views.py
@@ -9,18 +9,7 @@ from django.views.generic import ListView, DetailView, TemplateView
 
 # Create your views here.
 
-'''def home(request,offset=1):
-    """ Главная страница"""
-    print(request.user)
-    if offset == 0 : offset = 1
-    quantity_per_page = 12
-    data = new_product()
-    data['products'] = Product.objects.order_by('-id').all()[(offset - 1) * quantity_per_page:offset * quantity_per_page]
-    data['count'] = Product.objects.all().count()
-    data['offset'],data['offset_min'],data['offset_max'] = offset_product(offset, data['count'], quantity_per_page)
 
-    return render(request, 'home.html', context=data)
-'''
 
 class ProductListView(ListView):
     model = Product
@@ -34,17 +23,13 @@ class ProductListView(ListView):
             return queryset.all()
         return queryset.filter(category=cat)
 
-    def get_context_data(self, **kwargs):
-        """ Добавляем данные для слайдера 'Последние поступления' """
-        context = super().get_context_data(**kwargs)
-        context['showcase_product'] = new_product()
-        return context
+    extra_context = {'showcase_product': new_product() }
 
 
 class ContactsListView(ListView):
     """ Страница контактов """
     model = PageBlock
-    template_name = 'contacts.html'
+    template_name = 'catalog/contacts.html'
     context_object_name = 'contact_text'
 
     def get_queryset(self):
@@ -63,12 +48,7 @@ class ProductDetailView(DetailView):
     """ Страница карточка товара"""
     model = Product
     context_object_name = 'product'
-
-    def get_context_data(self, **kwargs):
-        """ Добавляем данные для слайдера 'Последние поступления' """
-        context = super().get_context_data(**kwargs)
-        context['showcase_product'] = new_product()
-        return context
+    extra_context = {'showcase_product': new_product() }
 
 
 class CategoryListView(ListView):
@@ -76,32 +56,8 @@ class CategoryListView(ListView):
     model = Category
     context_object_name = 'categories'
 
-    def get_context_data(self, **kwargs):
-        """ Добавляем данные для слайдера 'Последние поступления' """
-        context = super().get_context_data(**kwargs)
-        context['showcase_product'] = new_product()
-        return context
+    extra_context = {'showcase_product': new_product() }
 
-
-'''def category(request, pk=False, offset=1):
-    """ Страница категорий товаров"""
-    # если не выбрана категория
-    if not pk:
-        data = new_product()
-        data['categories'] = Category.objects.all()
-        return render(request, 'category.html', context=data)
-    # выбрана категория pk
-    else:
-        data = new_product()
-        quantity_per_page = 12
-        data['products'] = Product.objects.order_by('-id').filter(category=pk)[
-                           (offset - 1) * quantity_per_page:offset * quantity_per_page]
-        data['count'] = Product.objects.filter(category=pk).count()
-        data['offset'], data['offset_min'], data['offset_max'] = offset_product(offset, data['count'],
-                                                                                quantity_per_page)
-        category = Category.objects.get(pk=pk)
-        data['title'] = category.category_name
-        return render(request, 'product_one_category.html', context=data)'''
 
 def error_404_view(request, exception):
     """ Страница 404"""
