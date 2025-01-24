ec8b65d7c7d1a2fb9486155d6cac173895b4958a
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2025-01-24 16:52:58 +0300

    add creating product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,22 +1,22 @@
 
 from . import views
 from django.urls import path
-from .views import CategoryListView, ProductDetailView, ContactsListView, ProductListView
-from .models import Category
+from catalog import views
 
 app_name = 'catalog'
 
 urlpatterns = [
     #path('home/', views.home, name='home'),
     #path('<int:offset>', views.home, name='home'),
-    path('', ProductListView.as_view(), name='product'),
-    path('contacts/', ContactsListView.as_view(), name='contacts'),
-    path('products/', ProductListView.as_view(), name='product_category'),
+    path('', views.ProductListView.as_view(), name='product'),
+    path('contacts/', views.ContactsListView.as_view(), name='contacts'),
+    path('products/', views.ProductListView.as_view(), name='product_category'),
     #/products/?category=
-    path('product/<int:pk>', ProductDetailView.as_view(), name='single'),
+    path('product/<int:pk>', views.ProductDetailView.as_view(), name='single'),
     #path('category/<int:pk>', views.category, name='category'),
     #path('category/', views.category, name='categories'),
-    path('category/', CategoryListView.as_view(), name='categories'),
+    path('category/', views.CategoryListView.as_view(), name='categories'),
+    path('create/',views.ProductCreateView.as_view(), name='create')
 ]
 
 # add a flag for
--- catalog/views.py
+++ catalog/views.py
@@ -2,13 +2,15 @@ from gc import get_objects
 from xml.dom import NotFoundErr
 
 from django.shortcuts import render, get_object_or_404
-from .models import  PageBlock, Product, Category
+from .models import PageBlock, Product, Category
 from catalog.src.new_products import new_product
 from catalog.src.offset import offset_product
 from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-
 
 
 class ProductListView(ListView):
@@ -16,14 +18,35 @@ class ProductListView(ListView):
     context_object_name = 'products'
     paginate_by = 12
 
+    # Фильтрация и сортировка товаров на странице
     def get_queryset(self):
-        cat = self.request.GET.get('category')
         queryset = super().get_queryset()
-        if not cat:
-            return queryset.all()
-        return queryset.filter(category=cat)
+        # параметры сортировки
+        if sort := self.request.GET.get('sort'):
+            sort = sort.split(',')
+        else:
+            sort = ['-id']
+        # фильтрация по категориям
+        if cat := self.request.GET.get('category'):
+            return queryset.filter(category=cat).order_by(*sort)
+        return queryset.all().order_by(*sort)
 
-    extra_context = {'showcase_product': new_product() }
+    extra_context = {'showcase_product': new_product()}
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView ):
+    login_url = "/admin/"
+    #redirect_field_name = "redirect_to"
+
+    model = Product
+    fields = ['name', 'description', 'image', 'category', 'price']
+    success_url = reverse_lazy('catalog:product')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'description', 'image', 'category', 'price']
+    success_url = reverse_lazy('catalog:product')
 
 
 class ContactsListView(ListView):
@@ -48,7 +71,7 @@ class ProductDetailView(DetailView):
     """ Страница карточка товара"""
     model = Product
     context_object_name = 'product'
-    extra_context = {'showcase_product': new_product() }
+    extra_context = {'showcase_product': new_product()}
 
 
 class CategoryListView(ListView):
@@ -56,10 +79,9 @@ class CategoryListView(ListView):
     model = Category
     context_object_name = 'categories'
 
-    extra_context = {'showcase_product': new_product() }
+    extra_context = {'showcase_product': new_product()}
 
 
 def error_404_view(request, exception):
     """ Страница 404"""
     return render(request, '404.html')
-
